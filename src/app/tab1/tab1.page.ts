802f8ed2caa1d919e02fba519b6dc2d282313b25
Sacol16 <santiago.colonia@uao.edu.co>
2025-04-06 11:50:53 -0500

    Pagina de inicio

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { NavigationButtonsComponent } from 'src/app/navigation-buttons/navigation-buttons.component'
+
 
 @Component({
   selector: 'app-tab1',
   standalone: true,
-  imports: [CommonModule, IonicModule, NavigationButtonsComponent],
+  imports: [CommonModule, IonicModule],
   templateUrl: './tab1.page.html',
   styleUrls: ['./tab1.page.scss'],
 })
-export class Tab1Page {}
+export class Tab1Page {
+  constructor(private router: Router) {}
+
+  irATab2() {
+    this.router.navigate(['/tabs/tab2']);
+  }
+}
+
